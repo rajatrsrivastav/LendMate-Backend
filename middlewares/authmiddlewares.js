2ec07e593dec957a9ab2885c53137f550d904d98
Rajat Srivastav <ratneshrsrivastav@gmail.com>
2025-09-17 18:50:53 +0530

    changes in .env for prod

--- middlewares/authmiddlewares.js
+++ middlewares/authmiddlewares.js
@@ -1,4 +1,4 @@
 function authMiddleware(req,res,next){
     next()
 }
-module.exports=authMiddleware
+module.exports=authMiddleware 
--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const PORT = process.env.PORT || 3000;
 const app= express()
 
 app.use(express.json());
-// Enable CORS for local development
-app.use(cors({ origin: ['http://localhost:5173'], credentials: false }));
+
+app.use(cors({
+  origin: [process.env.CLIENT_URL, process.env.PROD_CLIENT_URL],
+  credentials: true 
+}));
 
 app.get('/',(req,res)=>{
     res.status(200).json({"Message":"Sever is running"});
